e8e53efb1664c2f03ab39e8164eabcdf5dd1d9c3
Daniel Knapp <dknapp@ucsd.edu>
2014-02-27 19:33:30 -0800

    CC Statistics
    Added code to the Main class and it's main method for accomplishing
    deliverable 1A

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.LinkedList;
+import java.io.*;
+import java.text.DecimalFormat;
 
 
 public class Main {
@@ -8,21 +10,67 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		computeCCs(5);
+//		computeCCs(20);
+//		computeCCs(100);
+//		computeCCs(500);
+		computeCCs(1000);
 			
 	}
 	
 	public static void computeCCs(int n) {
 		
-		double times[] = new double[51];
+//		double times[] = new double[51];
 		double p = 0;
+		Stats timer = new Stats();
 		
+		DecimalFormat fmt = new DecimalFormat("#.##");
 		
-		for(int i = 0; i <= 50; i++) {
-			Graph g = new Graph(n, p);
+		BufferedWriter runTimeOut;
+		BufferedWriter numCCOut;
+		
+		try
+		{
+			runTimeOut = new BufferedWriter(new FileWriter(new File(n + "ccRunTimes")));
+			numCCOut = new BufferedWriter(new FileWriter(new File(n + "ccStats")));
 			
-			p = p + .02;
+			
+			runTimeOut.write("#p runTime\n");
+			numCCOut.write("#p mean stdDev\n");
+			
+			for(int i = 0; i <= 50; i++) {
+				System.out.println("n = " + n + ", p = " + p);
+				timer.resetTimes();
+				timer.start();
+				
+				long connComps[] = new long[500];
+				for (int j = 0; j < 500	; j++)
+				{
+					Graph g = new Graph(n, p);
+					
+					LinkedList<Integer[]> cc = CCFinder.findCCs(g.adjacencyMatrix);
+					connComps[j] = cc.size();
+				}
+				
+				double mean = Stats.calcMean(connComps);
+				double stdDev = Stats.stdDev(connComps);
+				
+				timer.end();
+				double runTime = timer.runTime();
+				
+				runTimeOut.write(fmt.format(p) + " " + runTime + "\n");
+				numCCOut.write(fmt.format(p) + " " + mean + " " + stdDev + "\n");
+				p = p + .02;
+			}
+			
+			runTimeOut.close();
+			numCCOut.close();
+		} 
+		catch (Exception e)
+		{
+			System.err.println("I got probs!");
 		}
+		
+		
 		
 		return;
 	}
