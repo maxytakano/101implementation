084085d213400fc144d4fe365df2f3fd19bb2838
Max Takano <maxytakano@gmail.com>
2014-03-14 01:59:58 -0700

    Adapted RandomPoints to use ArrayList

--- src/RandomPoints.java
+++ src/RandomPoints.java
@@ -1,5 +1,6 @@
 
-import java.util.Random;
+import java.util.*;
+import java.util.List;
 import java.lang.Math;
 import javax.swing.*;
 import java.awt.*;
@@ -10,22 +11,24 @@ import java.awt.geom.Point2D;
  * generated point they represent.
  */
 public class RandomPoints extends JFrame{
-	int n;
-	Point2D.Double[] points;
+	private int n;
+	private List<Point2D.Double> points = new ArrayList<Point2D.Double>();
+	
 	Random randomD = new Random();
-	double theta;
-	double radius;
+	private double theta;
+	private double radius;
+	private List<Point2D.Double> hullPoints = new ArrayList<Point2D.Double>();
 	
 	public RandomPoints(int number) {
 		n = number;
-		points = new Point2D.Double[n];
 	}
 	
 	public void paint(Graphics g) {
 		g.setColor(Color.black);
-		for (int i = 0; i < points.length; i++) {
-			g.fillOval( ((int)(points[i].x * 100)) + 400, 
-					((int)(points[i].y * 100)) + 400, 2, 2);
+		
+		for (int i = 0; i < points.size(); i++) {
+			g.fillOval( ((int)(points.get(i).x * 100)) + 400, 
+				((int)(points.get(i).y * 100)) + 400, 2, 2);
 		}
 		
 	}
@@ -35,20 +38,31 @@ public class RandomPoints extends JFrame{
 		random.setSize(800,800);
 		random.generateRandomPoints();
 		
+		GrahamScan g = new GrahamScan();
+		random.hullPoints = g.getConvexHull(random.points);
+		
+		random.deleteHullPoints(random.points, random.hullPoints);
+		
+		random.repaint();
+		
 		random.setVisible(true);
 		
+	}
+	
+	private void deleteHullPoints(List<Point2D.Double> points, List<Point2D.Double> hull) {
+		for (int i = 0; i < hull.size(); i++) {
+			points.remove(hull.get(i));
+		}
 	}
 	
 	private void generateRandomPoints() {
 		for (int i = 0; i < n; i++) {
 			theta = 2*Math.PI*randomD.nextDouble();
 			radius = randomD.nextDouble();
-			points[i] = new Point2D.Double((double) radius*Math.cos(theta), 
-					(double) radius*Math.sin(theta));
+			points.add(new Point2D.Double((double) radius*Math.cos(theta), 
+					(double) radius*Math.sin(theta)));
 		}
 	}
-	
-	
 	
 	
 }
